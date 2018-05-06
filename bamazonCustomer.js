4c32e2beb1717ffc40057984d2b7ac5da652a7b3
Nelson Quinones <nelson.quinones@yahoo.com>
2018-05-06 15:28:43 -0400

    look fixed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,51 +6,26 @@ var http = require("http")
 
 var connection = mysql.createConnection({
     host: "localhost",
-    // Your username
+    //my username
     user: "root",
-  
-    // Your password
+    //my password
     password: "1234",
     database: "bamazon"
   });
 
-  var PORT = 8080;
-  var server = http.createServer();
+var PORT = 8080;
+var server = http.createServer();
 
-  ////////////////////////////////////////////////////
-  //this function is for to connect to mysql
-  connection.connect(function(err) {
+///////////////////////////////////////this function is for to connect to mysql
+connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     getTable();
-  });
+    });
 
 
 
-  //this is where i'm suppoosed to see the data come back
-  function getTable() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-    
-        // Log all results of the SELECT statement
-        console.log(res);
-        connection.end();
-      });
-
-
-  }
-  
-// /////////////////////////////////////////////clean look ///////
-//   function cleanLook () {
-//     var products = ["item_id", "Product_name", "department_name", "price", "stock_quantity"];
-//     products.toString();
-//     document.getElementById("demo").innerHTML = products;
-// }
-
-
-
-
-////////////////////////////////////////////////////
+///////////////////////////this is where i'm suppoosed to see the data come back..
 //console log: What is the ID of the product they would like to buy?
 //console log: how many units of the product they would like to buy?
 //console.log `Insufficient quantity!
@@ -59,6 +34,25 @@ var connection = mysql.createConnection({
 //now i'm just getting raw data
 //isolate data from the array
 //object orientation
+
+
+  function getTable() {
+    connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    
+//  Log all results of the SELECT statement
+//  console.log(res);
+
+
+ for (var i = 0; i < res.length; i++) {
+ console.log("item_id: " + res[i].item_id + " || product_name: " + res[i].product_name + " || department_name: " + res[i].department_name + " || price: " + res[i].price + " || stock_quantity: " + res[i].stock_quantity);
+}
+
+connection.end();
+});
+}
+////////////////////////////////////////////////////
+
 
 
 var Order = function() {
