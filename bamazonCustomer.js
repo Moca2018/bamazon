527a8efabf405d0d593beea15090bd672bc2a5bf
Nelson Quinones <nelson.quinones@yahoo.com>
2018-05-06 17:12:44 -0400

    better look

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,13 @@
-//The following is the logic of bamazon
+
+
+//////////////////////////The following is the logic of bamazon/////////////////////////
 
 // Require/import the HTTP module
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var http = require("http");
-
+var PORT = 8080;
+var server = http.createServer(handleRequest);
 var connection = mysql.createConnection({
     host: "localhost",
     //my username
@@ -14,10 +17,8 @@ var connection = mysql.createConnection({
     database: "bamazon"
   });
 
-var PORT = 8080;
-var server = http.createServer(handleRequest);
+///////////////////////////function to connect to mysql/////////////////////////////////////////
 
-///////////////////////////////////////this function is for to connect to mysql
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
@@ -26,88 +27,30 @@ connection.connect(function(err) {
 
 
 
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+////////////////////////////////function to handle requests and responses//////////////////////
 
-    // Send the below string to the client when the user visits the PORT URL
-    response.end("It Works!! Path Hit: " + request.url);
+
+function handleRequest(request, response) {
+// Send the below string to the client when the user visits the PORT URL
+response.end("It Works!! Path Hit: " + request.url);
   }
 
-///////////////////////////this is where i'm suppoosed to see the data come back..
-//console log: What is the ID of the product they would like to buy?
-//console log: how many units of the product they would like to buy?
-//console.log `Insufficient quantity!
-//res is an array of objects
-//Refine the look of the data
-//now i'm just getting raw data
-//isolate data from the array
-//object orientation
 
+////////////////////////////////function to display products ////////////////////////////////
 
-  function getTable() {
+function getTable() {
     connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     
 //  Log all results of the SELECT statement
 //  console.log(res);
-
-
  for (var i = 0; i < res.length; i++) {
  console.log("item_id: " + res[i].item_id + " || product_name: " + res[i].product_name + " || department_name: " + res[i].department_name + " || price: " + res[i].price + " || stock_quantity: " + res[i].stock_quantity);
 }
-
-connection.end();
+order();
 });
 }
-////////////////////////////////////////////////////
-
-
-
-var Order = function() {
-  inquirer.prompt([{
-      name: "ProductID",
-      type: "input",
-      message: "What is the ID of the product that you would like to buy?",
-      
-      //Validation goes here
-      validate: function(value) {
-          if (isNaN(value) == false) {
-              return true;
-          } else {
-              return false;
-          }
-      }
-  }, {
-      name: "Quantity",
-      type: "input",
-      message: "how many units of the product they would like to buy?",
-      validate: function(value) {
-          if (isNaN(value) == false) {
-              return true;
-          } else {
-              return false;
-          }
-      }
-  }]).then(function(answer) {
-
-////////////////////////////////////////////////////
-
-var query = 'SELECT * FROM Products WHERE itemID=' + answer.Quantity;
-connection.query(query, function(err, res) {
-  if (answer.Quantity <= res) {
-    for (var i = 0; i < res.length; i++) {
-        console.log("We have available " + res[i].stockQuantity + " " + res[i].productName + ".");
-        console.log("Your order of "+ res[i].stockQuantity + " " + res[i].productName + " is now processing!");
-      }
-    } else {
-      console.log("Insufficient quantity!");
-    }
-    displayProducts();
-})
-})
-connection.end();
-};
-
+//////////////////////////////function to listen to the Port ////////////////////////////////
 
 
 
@@ -115,3 +58,52 @@ connection.end();
 server.listen(PORT, function() {
   console.log("Server is listening on PORT: " + PORT);
 });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+var order = function() {
+    inquirer.prompt([{
+        name: "ProductID",
+        type: "input",
+        message: "What is the ID of the product that you would like to buy?",
+        
+        //Validation goes here
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }, {
+        name: "Quantity",
+        type: "input",
+        message: "how many units of the product they would like to buy?",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+  
+  //////////////////////////////////////////////////// Display all products
+  
+  var query = 'SELECT * FROM Products WHERE itemID=' + answer.Quantity;
+  connection.query(query, function(err, res) {
+    if (answer.Quantity <= res) {
+      for (var i = 0; i < res.length; i++) {
+          console.log("We have available " + res[i].stockQuantity + " " + res[i].productName + ".");
+          console.log("Your order of "+ res[i].stockQuantity + " " + res[i].productName + " is now processing!");
+        }
+      } else {
+        console.log("Insufficient quantity!");
+      }
+      displayProducts();
+  })
+  })
+  connection.end();
+  };
+  
